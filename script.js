f630d9b0a0acf5a3fc0a97345f084f402b8a188e
Annemette <anne7n62@stud.kea.dk>
2021-02-12 20:48:33 +0100

    not sure what's going on but it's going somewhere

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const allStudents = [];
 //making a prototype for student objects
 const Student = {
     firstname: "",
-    middlename: "unknown",
+    middlename: "",
     lastname: "",
-    nickname: "unknown",
+    nickname: "",
     photofilename: "",
     house: "",
 }
@@ -46,18 +46,36 @@ student.firstname = studentFirstName;
 
 // middlename
 let studentMiddleName = studentData[1];
-console.log(studentMiddleName);
-student.middlename = studentMiddleName;
+    console.log(studentMiddleName);
+    student.lastname = studentMiddleName;
 
 // lastname
 let studentLastName = studentData[2];
 console.log(studentLastName);
 student.lastname = studentLastName;
 
+//Gøre første bogstav stort - firstname
+student.firstNameCapital =
+student.firstname.substring(0, 1).toUpperCase();
+//Gøre første bogstav stort - middlename
+student.middleNameCapital =
+student.middlename.substring(0, 1).toUpperCase();
+//Gøre første bogstav stort - lastname - virker ikke
+// student.lastNameCapital =
+// student.lastname.substring(0, 1).toUpperCase();
+
 // house
 let studentHouse = jsonObject.house;
 console.log(studentHouse);
 student.house = studentHouse;
+
+//Nickname
+//let studentNickName = 
+// console.log(studentHouse);
+//student.nickname = studentNickName;
+
+//  photofilename: "",
+  
 
 //Adding all the objects into the array
 allStudents.push(student);
