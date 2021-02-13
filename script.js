dabb0d03a0cb9bc3b8e7977b95808810621b6144
Annemette <anne7n62@stud.kea.dk>
2021-02-13 17:26:13 +0100

    It's not perfect

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const Student = {
     middlename: "",
     lastname: "",
     nickname: "",
-    photofilename: "",
+    gender: "",
     house: "",
-}
+};
 
 function start(){
     console.log("ready");
@@ -22,62 +22,93 @@ function start(){
 
 //fetching the json file
 function loadJSON(){
-fetch ("students.json")
-.then( response => response.json() )
+fetch ("https://petlatkea.dk/2021/hogwarts/students.json")
+.then(response => response.json())
 .then( jsonData => {
     //When loaded, clean objects
-    cleanObjects(jsonData);
+    prepareObjects(jsonData);
 });
+console.log("loaded");
 } 
 
-function cleanObjects(jsonData) {
-jsonData.forEach( jsonObject => {
+function prepareObjects(jsonData) {
+jsonData.forEach(jsonObject => {
 
-//create object
-const student = Object.create(Student);
-//splitting fullname by spaces
-let studentData = jsonObject.fullname.split(" ");
-console.log(studentData);
+//create student object
+const aStudent = Object.create(Student);
 
-// firstname
-let studentFirstName = studentData[0];
-console.log(studentFirstName);
-student.firstname = studentFirstName;
+//Finde navnene ved at definerer mellemrum
+const firstSpace = jsonObject.fullname.trim().indexOf(" ");
+const lastSpace = jsonObject.fullname.trim().lastIndexOf(" ");
 
-// middlename
-let studentMiddleName = studentData[1];
-    console.log(studentMiddleName);
-    student.lastname = studentMiddleName;
+//Definere navne / adskille dem ved mellemrum
+aStudent.firstName = jsonObject.fullname.trim().substring (0,firstSpace);
+aStudent.middleName = jsonObject.fullname.substring(firstSpace,lastSpace);
 
-// lastname
-let studentLastName = studentData[2];
-console.log(studentLastName);
-student.lastname = studentLastName;
+if (aStudent.middleName.includes('"')) {
+    aStudent.nickName = aStudent.middleName;
+    aStudent.middleName = "";
+}
 
+aStudent.lastName = jsonObject.fullname.trim().substring(lastSpace).trim();
+
+//______________
 //Gøre første bogstav stort - firstname
-student.firstNameCapital =
-student.firstname.substring(0, 1).toUpperCase();
-//Gøre første bogstav stort - middlename
-student.middleNameCapital =
-student.middlename.substring(0, 1).toUpperCase();
-//Gøre første bogstav stort - lastname - virker ikke
-// student.lastNameCapital =
-// student.lastname.substring(0, 1).toUpperCase();
+aStudent.firstNameCaptial = aStudent.firstName.substring(0, 1).toUpperCase() + aStudent.firstName.substring(1, firstSpace).toLowerCase();
+// //Gøre første bogstav stort - middlename
+aStudent.middleNameCaptial = aStudent.middleName.substring(0, 1).toUpperCase() + aStudent.middleName.substring(1, aStudent.middleName.length).toLowerCase(aStudent.middleName.length);
+// //Gøre første bogstav stort - lastname 
+aStudent.lastNameCaptial = aStudent.lastName.substring(0,1).toUpperCase() + aStudent.lastName.substring(1).toLowerCase(aStudent.middleName.length);
+// //Gøre første bogstav stort - nickname
+aStudent.middleNameCapital = aStudent.middleName.substring(0, 1).toUpperCase() + aStudent.middleName.substring(1).toLowerCase(aStudent.middleName.length);
+
+// gender
+aStudent.gender = jsonObject.gender.substring(0).trim();
+    aStudent.genderCapital =
+      aStudent.gender.substring(0, 1).toUpperCase() +
+      aStudent.gender.substring(1).toLowerCase();
 
 // house
-let studentHouse = jsonObject.house;
-console.log(studentHouse);
-student.house = studentHouse;
-
-//Nickname
-//let studentNickName = 
-// console.log(studentHouse);
-//student.nickname = studentNickName;
-
-//  photofilename: "",
-  
+aStudent.house = jsonObject.house.substring(0).trim();
+aStudent.houseCapital =
+  aStudent.house.substring(0, 1).toUpperCase() +
+  aStudent.house.substring(1).toLowerCase();
 
 //Adding all the objects into the array
-allStudents.push(student);
+aStudent.firstName = aStudent.firstNameCapital;
+aStudent.middleName = aStudent.middleNameCapital;
+aStudent.lastName = aStudent.lastNameCapital;
+aStudent.nickName = aStudent.nickNameCapital;
+aStudent.gender = aStudent.genderCapital;
+aStudent.house = aStudent.houseCapital;
+allStudents.push(aStudent);
+console.log(aStudent);
 });
+
+displayList();
 }
+
+function displayList() {
+    //clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+
+    //build a new list
+    allStudents.forEach(displayStudent);
+}
+
+function displayStudent (student) {
+    //create clone
+    const klon = document.querySelector("template#student").content.cloneNode(true);
+
+    //set clone data
+    klon.querySelector("[data-field=firstname]").textContent = student.firstName;
+    klon.querySelector("[data-field=lastname]").textContent = student.lastName;
+    klon.querySelector("[data-field=middlename]").textContent = student.middleName;
+    klon.querySelector("[data-field=nickname]").textContent = student.nickName;
+    klon.querySelector("[data-field=house]").textContent = student.house;
+
+    // append clone to list
+    document.querySelector("#list tbody").appendChild(klon);
+
+}
+
